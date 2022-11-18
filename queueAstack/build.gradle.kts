ecbe0637a146445960f87aaf764ad49631acbdb4
kleonov <kirill.leonov@yahoo.com>
2022-11-18 11:56:50 +0700

    add queueAstack module

---
+++ queueAstack/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "1.7.20"
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
