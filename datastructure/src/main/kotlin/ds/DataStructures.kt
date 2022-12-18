c492ab010ff1ee42b0bd59a889065757d92ba01b
kleonov <kirill.leonov@yahoo.com>
2022-12-18 13:28:03 +0800

    add datastructure module

---
+++ datastructure/src/main/kotlin/ds/DataStructures.kt
@@ -0,0 +1,14 @@
+package ds
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+
+    override fun toString(): String {
+        return "ListNode(`val`=$`val`)"
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
